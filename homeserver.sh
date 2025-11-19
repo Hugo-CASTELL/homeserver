ef9d125d710197127cbd3b677568dd7399f64e9c
Hugo-CASTELL <hugo.castell@outlook.fr>
2025-11-19 08:33:07 +0100

    Ordered docker containers start

--- homeserver.sh
+++ homeserver.sh
@@ -10,7 +10,7 @@ fi
 
 for should_decrypt_file in $(cat .gitattributes | cut -d' ' -f1 | xargs); do
   if cat "$should_decrypt_file" | grep -q "^GITCRYPT"; then
-    if [ -z key ]; then
+    if [ -f key ]; then
       git-crypt unlock ./key
     else
       echo "Should decrypt a file using git-crypt but no key provided as 'key' file at repo root"
@@ -19,21 +19,44 @@ for should_decrypt_file in $(cat .gitattributes | cut -d' ' -f1 | xargs); do
   fi
 done
 
-COMPOSE_FOLDERS="services/* cloudflared"
+SERVICES_FOLDER="services/*"
+DELAYED="services/nginx-reverse-proxy cloudflared"
+
+up() {
+  (cd "$1" && docker compose up --detach)
+}
+
+down() {
+  (cd "$1" && docker compose down)
+}
 
 if [ "$1" = "up" ]; then
+
   docker network create homeserver-net
 
-  for folder in $(echo $COMPOSE_FOLDERS | tac -s ' '); do
-    if [ -d "$folder" ]; then
-      (cd "$folder" && docker compose up --detach)
+  for folder in services/*; do
+    if [ "$folder" = "services/nginx-reverse-proxy" ]; then
+      continue
+    elif [ -d "$folder" ]; then
+      up $folder
     fi
   done
 
+  for folder in $(echo $DELAYED | tac -r -s ' ' | tac); do
+    up $folder
+  done
+
 elif [ "$1" = "down" ]; then
-  for folder in $(echo $COMPOSE_FOLDERS | tac -s ' '); do
-    if [ -d "$folder" ]; then
-      (cd "$folder" && docker compose down)
+
+  for folder in $(echo $DELAYED | tac -s ' '); do
+    down $folder
+  done
+
+  for folder in services/*; do
+    if [ "$folder" = "services/nginx-reverse-proxy" ]; then
+      continue
+    elif [ -d "$folder" ]; then
+      down $folder
     fi
   done
 
